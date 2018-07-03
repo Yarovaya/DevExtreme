f82c27111176c2855b5941ed3a7605f85043a87f
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-07-03 15:03:05 +0300

    Fix some tests

--- js/ui/scheduler/ui.scheduler.work_space.js
+++ js/ui/scheduler/ui.scheduler.work_space.js
@@ -2016,6 +2016,7 @@ var SchedulerWorkSpace = Widget.inherit({
         return {
             top: position.top + shift.top,
             left: position.left + shift.left,
+            sortTop: position.top + shift.sortTop,
             rowIndex: position.rowIndex,
             cellIndex: position.cellIndex,
             hMax: this._groupedStrategy.getHorizontalMax(groupIndex),
