008bdce923a21bde2d22dc7aac5f2b9e128e2772
Anton Sermyazhko <pin361@gmail.com>
2020-07-08 12:58:45 +0300

    Check license using indexOf

--- build/gulp/check_licenses.js
+++ build/gulp/check_licenses.js
@@ -5,17 +5,17 @@ const named = require('vinyl-named');
 
 const checkRruleLicenseNotice = lazyPipe()
     .pipe(named, function(file) {
-        const name = 'rrule.js';
         const url = 'https://github.com/jakubroztocil/rrule';
         const licenseUrl = 'https://github.com/jakubroztocil/rrule/blob/master/LICENCE';
-        const re = new RegExp('\\*!\\s*.*' + name + '[\\s\\S]*' + url + '[\\s\\S]*' + '.*Copyright[\\s\\S]*' + licenseUrl);
+        const copyright = 'Copyright 2010, Jakub Roztocil and Lars Schoning\\n * Licenced under the BSD licence.';
 
-        if(file.contents.toString().search(re) === -1) {
+        const text = `${url}\\n *\\n * ${copyright}\\n * ${licenseUrl}`;
+        if(file.contents.toString().indexOf(text) === -1) {
             throw new Error(`RRule license header wasn't found in ${file.stem}`);
         }
     });
 
 gulp.task('check-license-notices', function() {
-    return gulp.src('artifacts/js/dx.all.js')
+    return gulp.src('artifacts/js/dx.all.debug.js')
         .pipe(checkRruleLicenseNotice());
 });
